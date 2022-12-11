7092f3415189f8b7b8dbcff76399cfd132a155ec
OuadieZerhouni <Ouadie.zerhouni@usmba.ac.ma>
2022-12-11 23:43:35 +0100

    .

--- src/main/java/Competition/Main.java
+++ src/main/java/Competition/Main.java
@@ -1,6 +1,5 @@
 package Competition;
 
-//import swing stuff
 import java.awt.*;
 import java.awt.event.*;
 import java.sql.SQLException;
@@ -8,7 +7,6 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        // create a frame
         JFrame frame = new JFrame("Competition");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(400, 400);
@@ -34,10 +32,25 @@ public class Main {
                 selectCompt.addItem(c.getNom());
             }
         }
+        JButton selectbtn = new JButton("Select");
+        selectbtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                try {
+                    LaodTeamInsert(frame);
+                } catch (SQLException ex) {
+                    throw new RuntimeException(ex);
+                }
+
+            }
+        });
         CompetitionPanel.add(selectComptLabel);
         CompetitionPanel.add(selectCompt);
+        CompetitionPanel.add(selectbtn);
         frame.add(CompetitionPanel);
         CompetitionPanel.setVisible(true);
+        
         
     }
     public static void DesPanels(JFrame frame) {
@@ -46,7 +59,6 @@ public class Main {
         }
     }
     
-    //LaodTeamInsert
     public static void LaodTeamInsert(JFrame frame) throws SQLException {
         DesPanels(frame);
         JPanel TeamInsertPanel = new JPanel();
@@ -54,16 +66,16 @@ public class Main {
         JLabel TeamInsertLabel1 = new JLabel("entrer ID");
         JTextField Id_txt = new JTextField();
         JLabel TeamInsertLabel2 = new JLabel("entrer Nom");
-        JtextField  Nom_txt = new JTextField();
+        JTextField  Nom_txt = new JTextField();
         JLabel TeamInsertLabel3 = new JLabel("entrer Pays");
-        JtextField  Pays_txt = new JTextField();
-        //input CL
+        JTextField  Pays_txt = new JTextField();
         JLabel TeamInsertLabel4 = new JLabel("entrer Classement");
-        JtextField  CL_txt = new JTextField();
-        TeamInsertText1.setPreferredSize(new Dimension(100, 20));
-        TeamInsertText2.setPreferredSize(new Dimension(100, 20));
-        TeamInsertText3.setPreferredSize(new Dimension(100, 20));
-        TeamInsertText4.setPreferredSize(new Dimension(100, 20));
+        JTextField  CL_txt = new JTextField();
+        Id_txt.setPreferredSize(new Dimension(100, 20));
+        Nom_txt.setPreferredSize(new Dimension(100, 20));
+        Pays_txt.setPreferredSize(new Dimension(100, 20));
+        CL_txt.setPreferredSize(new Dimension(100, 20));
+
         TeamInsertPanel.add(TeamInsertLabel1);
         TeamInsertPanel.add(Id_txt); 
         TeamInsertPanel.add(TeamInsertLabel4);
