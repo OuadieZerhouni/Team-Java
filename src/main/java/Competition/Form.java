f01b370b2b4298530a587fcfba1075338570f467
OuadieZerhouni <Ouadie.zerhouni@usmba.ac.ma>
2022-12-13 12:29:56 +0100

    .

---
+++ src/main/java/Competition/Form.java
@@ -0,0 +1,11 @@
+package Competition;
+
+import javax.swing.*;
+
+public class Form {
+    private JTextArea textArea1;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
